80008fe5d0d16dc0767346f94d289b850bf82e93
Ikhwanul Akhmad. DLY <heyikhwan@gmail.com>
2025-10-03 21:58:12 +0700

    feat. chat component saat belum ada chat

--- src/components/pages/chat/chat-bot.tsx
+++ src/components/pages/chat/chat-bot.tsx
@@ -45,15 +45,29 @@ const ChatBot = () => {
 
     return (
         <div className={`w-full lg:max-w-3xl mx-auto flex flex-col ${messages.length > 0 ? "" : "my-auto"}`}>
-            <main className="p-4 flex-1 overflow-y-auto pb-32">
-                <Chat messages={messages} isTyping={isTyping} />
-            </main>
+            {messages.length > 0 ? (
+                <>
+                    <main className="p-4 flex-1 overflow-y-auto pb-32">
+                        <Chat messages={messages} isTyping={isTyping} />
+                    </main>
 
-            <div className="fixed bottom-0 left-0 right-0 bg-background">
-                <div className="mx-auto w-full lg:max-w-3xl px-4 py-3">
-                    <InputChat onSendMessage={handleSendMessage} isTyping={isTyping} />
+                    <div className="fixed bottom-0 left-0 right-0 bg-background">
+                        <div className="mx-auto w-full lg:max-w-3xl px-4 py-3">
+                            <InputChat onSendMessage={handleSendMessage} isTyping={isTyping} />
+                        </div>
+                    </div>
+                </>
+            ) : (
+                <div className="flex flex-1 flex-col items-center justify-center text-center px-4 h-screen">
+                    <div className="mb-8">
+                        <h2 className="font-semibold text-2xl">Apa hal yang ingin Anda diskusikan hari ini ?</h2>
+                    </div>
+
+                    <div className="w-full lg:max-w-3xl">
+                        <InputChat onSendMessage={handleSendMessage} isTyping={isTyping} />
+                    </div>
                 </div>
-            </div>
+            )}
         </div>
     )
 }
